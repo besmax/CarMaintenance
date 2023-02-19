79a252303555be9e2561f0c2b7283b99d921a94c
Max <bespalov.m.9@ya.ru>
2023-02-19 15:08:53 +0300

    Initial commit

---
+++ app/src/main/java/bes/max/carmaintenance/model/Check.kt
@@ -0,0 +1,15 @@
+package bes.max.carmaintenance.model
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+data class Check(
+    var checkId: Long = 0L,
+    var checkName: String = "",
+    var checkMileage: Long = 0L,
+    var checkDate: LocalDate = LocalDate.parse("31-12-2018",
+        DateTimeFormatter.ofPattern("dd-MM-yyyy")),
+    var checkPrice: Long = 0L,
+    var checkCompany: String = ""
+
+)
