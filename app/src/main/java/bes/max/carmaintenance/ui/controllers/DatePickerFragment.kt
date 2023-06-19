ea15fc859c8c1566a258493369d20517aacf59d6
Max <bespalov.m.9@ya.ru>
2023-06-19 20:08:05 +0300

    add to calendar functionality was added for new check fragment, date in calendar not correct

--- app/src/main/java/bes/max/carmaintenance/ui/controllers/DatePickerFragment.kt
+++ app/src/main/java/bes/max/carmaintenance/ui/controllers/DatePickerFragment.kt
@@ -2,11 +2,13 @@ package bes.max.carmaintenance.ui.controllers
 
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import bes.max.carmaintenance.BaseApplication
+import bes.max.carmaintenance.R
 import bes.max.carmaintenance.ui.viewmodels.ChecksViewModel
 import bes.max.carmaintenance.ui.viewmodels.ChecksViewModelFactory
 import java.util.Calendar
@@ -30,5 +32,10 @@ class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener 
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
         viewModel.date.value = "$dayOfMonth.${month+1}.$year"
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+        viewModel.date.value = getString(R.string.fragment_new_check_choose_date)
     }
 }
--- app/src/main/java/bes/max/carmaintenance/ui/controllers/NewCheckFragment.kt
+++ app/src/main/java/bes/max/carmaintenance/ui/controllers/NewCheckFragment.kt
@@ -1,18 +1,27 @@
 package bes.max.carmaintenance.ui.controllers
 
+import android.content.Intent
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import bes.max.carmaintenance.BaseApplication
+import bes.max.carmaintenance.R
 import bes.max.carmaintenance.databinding.FragmentNewCheckBinding
 import bes.max.carmaintenance.ui.viewmodels.ChecksViewModel
 import bes.max.carmaintenance.ui.viewmodels.ChecksViewModelFactory
 import bes.max.carmaintenance.ui.viewmodels.NewCheckViewModel
 import bes.max.carmaintenance.ui.viewmodels.NewCheckViewModelFactory
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class NewCheckFragment : Fragment() {
 
@@ -42,24 +51,29 @@ class NewCheckFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fragmentNewCheckChooseDate.setText(R.string.fragment_new_check_choose_date)
+
         binding.fragmentNewCheckChooseDate.setOnClickListener {
             showDatePickerDialog(view)
             binding.fragmentNewCheckChooseDate.setText(viewModel.date?.value)
         }
 
         viewModel.date.observe(viewLifecycleOwner) {
-            binding.fragmentNewCheckChooseDate.setText(viewModel.date?.value)
+            if (!it.isNullOrEmpty()) {
+                binding.fragmentNewCheckChooseDate.setText(viewModel.date?.value)
+            }
         }
 
         binding.fragmentNewCheckButton.setOnClickListener {
-            if (!binding.fragmentNewCheckEditText.text.isNullOrEmpty() && viewModel.date?.value != null) {
-                newCheckViewModel.insertPlannedCheck(
-                    binding.fragmentNewCheckEditText.text.toString(),
-                    viewModel.date.value!!
-                )
-                binding.fragmentNewCheckEditText.text.clear()
+            val plannedCheckIsInserted = insertPlannedCheck()
+            if (plannedCheckIsInserted && binding.fragmentNewCheckCheck.isChecked) {
+                addToCalendar()
             }
-
+            if (plannedCheckIsInserted) {
+                binding.fragmentNewCheckEditText.text.clear()
+                viewModel.date?.value = getString(R.string.fragment_new_check_choose_date)
+                binding.fragmentNewCheckCheck.isChecked = false
+            }
         }
 
     }
@@ -68,6 +82,43 @@ class NewCheckFragment : Fragment() {
         val newFragment = DatePickerFragment()
         newFragment.show(childFragmentManager, "datePicker")
     }
+
+    private fun insertPlannedCheck(): Boolean {
+        if (!binding.fragmentNewCheckEditText.text.isNullOrEmpty() &&
+            viewModel.date?.value != null &&
+            viewModel.date?.value != getString(R.string.fragment_new_check_choose_date)
+        ) {
+            newCheckViewModel.insertPlannedCheck(
+                binding.fragmentNewCheckEditText.text.toString(),
+                viewModel.date.value!!
+            )
+            return true
+        } else {
+            Toast.makeText(requireContext(), R.string.fragment_new_check_error, Toast.LENGTH_LONG)
+                .show()
+            return false
+        }
+    }
+
+    private fun addToCalendar() {
+        if (!binding.fragmentNewCheckEditText.text.isNullOrEmpty() &&
+            viewModel.date?.value != null &&
+            viewModel.date?.value != getString(R.string.fragment_new_check_choose_date)
+        ) {
+            val dateList = viewModel.date?.value!!.split(".")
+            val begin: Long = Calendar.getInstance().run {
+                set(dateList[0].toInt(), dateList[1].toInt(), dateList[2].toInt(), 7, 0)
+                timeInMillis
+            }
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+                .putExtra(CalendarContract.Events.TITLE, "CarMaintenance")
+                .putExtra(CalendarContract.Events.DESCRIPTION, binding.fragmentNewCheckEditText.text.toString())
+            startActivity(intent)
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
